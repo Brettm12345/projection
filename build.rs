a85fe359ae03857907b51e82837c9f91449859a4
brettm12345 <brettmandler@gmail.com>
2020-02-21 02:18:06 -0500

    Added elvish and powershell support

--- build.rs
+++ build.rs
@@ -7,6 +7,8 @@ fn main() {
     let mut app = build_cli();
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let mut gen = |shell| app.gen_completions("projection", shell, out_dir.clone());
+    gen(Shell::Elvish);
+    gen(Shell::PowerShell);
     gen(Shell::Bash);
     gen(Shell::Fish);
     gen(Shell::Zsh);
